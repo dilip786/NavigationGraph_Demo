ff5b9679a6bb4c92374dcb9842f46447d6f64825
touchnoteuser2 <dilip@touchnote.com>
2019-12-19 16:35:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationgraph_demo/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navigationgraph_demo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.first_fragment, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+
+            view.findNavController().navigate(R.id.fragmentFirstToSecond)
+        }
+
+    }
+}
